0a37edc57663fdc353d7c2188712b5c9e1324473
brdeleuran <46991809+brdeleuran@users.noreply.github.com>
2019-01-24 10:20:00 +0100

    Add files via upload

---
+++ Rumfang/Rumfang/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rumfang
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Indtast længde: ");
+            double length = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Indtast højde: ");
+            double height = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Indtast bredde: ");
+            double width = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Rumfanget = " + (length*height*width) + " m³");
+            Console.ReadKey();
+        }
+    }
+}
