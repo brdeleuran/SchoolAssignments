73e3d1fd79c33d43367f7649fa56d05998b9c8b2
brdeleuran <46991809+brdeleuran@users.noreply.github.com>
2019-01-24 10:31:07 +0100

    Add files via upload

---
+++ TerningeKast/TerningeKast/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerningeKast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int terningekast = random.Next(1, 7);
+
+            if (terningekast == 1)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Du slog en etter!");
+            }
+
+            else if (terningekast == 2)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Du slog en toer!");
+            }
+
+            else if (terningekast == 3)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine("Du slog en tréer!");
+            }
+
+            else if (terningekast == 4)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine("Du slog en firer!");
+            }
+
+            else if (terningekast == 5)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Du slog en femmer!");
+            }
+
+            else if (terningekast == 6)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkBlue;
+                Console.WriteLine("Du slog en sekser!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
