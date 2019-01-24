8ff7dd8a3a3ca22cd509d964b622ddb2f6a84097
brdeleuran <46991809+brdeleuran@users.noreply.github.com>
2019-01-24 10:54:42 +0100

    Add files via upload

---
+++ Pythagoras/Pythagoras/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pythagoras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter a second number: ");
+            double b = double.Parse(Console.ReadLine());
+
+            double c2 = ((a * a) + (b * b));
+            double c = Math.Sqrt(c2);
+
+            Console.WriteLine(a + "² + " + b + "² = " + c2);
+            Console.WriteLine("c = " + c);
+
+            Console.WriteLine("");
+
+            if (a < b)
+                Console.WriteLine(b);
+            else
+                Console.WriteLine(a);
+
+            Console.ReadKey();
+        }
+    }
+}
