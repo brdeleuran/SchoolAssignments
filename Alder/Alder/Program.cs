82a640f087b1d798182a8e734e16a93165ce102e
brdeleuran <46991809+brdeleuran@users.noreply.github.com>
2019-01-24 11:38:15 +0100

    Add files via upload

---
+++ Alder/Alder/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(allowed());
+            Console.ReadKey();
+        }
+
+        static string allowed() //If-Else method that tests what you are allowed to do depending on your input age
+        {
+            Console.WriteLine("Enter name: ");
+            string inputName = Console.ReadLine();
+
+            Console.WriteLine("Enter age: ");
+            string inputAge = Console.ReadLine();
+
+            while (!int.TryParse(inputAge, out int result))
+            {
+                Console.WriteLine("Please enter a numeric value.");
+                Console.WriteLine("Enter age: ");
+                inputAge = Console.ReadLine();
+
+            }
+
+            int intInputAge = Convert.ToInt32(inputAge);
+
+            string showResult = "";
+
+            if (intInputAge < 3)
+                showResult = inputName + ", du må gå med ble.";
+
+            else if (intInputAge <= 15)
+                showResult = inputName + ", du må ingenting.";
+
+            else if (intInputAge <= 17)
+                showResult = inputName + ", du må drikke.";
+
+            else
+                showResult = inputName + ", du må stemme og køre bil.";
+
+            return showResult;
+        }
+    }
+}
