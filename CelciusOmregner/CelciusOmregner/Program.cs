cd65517a59ae6990a6ea915bd1c78881bbd6180f
brdeleuran <46991809+brdeleuran@users.noreply.github.com>
2019-01-24 09:55:51 +0100

    Add files via upload

---
+++ CelciusOmregner/CelciusOmregner/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CelciusOmregner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double celcius;
+
+            Console.WriteLine("Enter degrees in Celcius: ");
+            celcius = double.Parse(Console.ReadLine());
+
+            double Reamur = celcius * 0.8;
+            double Fahrenheit = celcius * 1.8 + 32;
+
+            Console.WriteLine("Degrees in Reamur: " + Reamur);
+            Console.WriteLine("Degrees in Fahrenheit: " + Fahrenheit);
+            Console.ReadKey();
+        }
+    }
+}
