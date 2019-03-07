7fd9b61fc20ee81c7813699bbc6d98dbc058d5e0
brdeleuran <46991809+brdeleuran@users.noreply.github.com>
2019-03-07 12:12:59 +0100

    Add files via upload

---
+++ Geometry/Geometry/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Square s1 = new Square(23.5);
+            Square s2 = new Square(23.5);
+            Square s3 = new Square(19.55);
+            Square s4 = new Square(58.12);
+
+            Console.WriteLine(s1.Perimeter());
+            Console.WriteLine(s2.Perimeter());
+            Console.WriteLine(s3.Size());
+            Console.WriteLine(s4.Size());
+            s1.Side = 100;
+            s2.Side = 200;
+            s3.Side = 300;
+            s4.Side = 400;
+            Console.WriteLine(s1.Perimeter());
+            Console.WriteLine(s2.Perimeter());
+            Console.WriteLine(s3.Size());
+            Console.WriteLine(s4.Size());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Geometry/Geometry/Square.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometry
+{
+    public class Square
+    {
+        private double side;
+
+        public double Side
+        {
+            get
+            {
+                return this.side;
+            }
+            set
+            {
+                this.side = value;
+            }
+
+        }
+
+        public Square()
+        {
+
+        }
+
+        public Square(double side)
+        {
+            this.side = side;
+        }
+
+        public double Perimeter()
+        {
+            double perimeter = side * 4;
+
+            return perimeter;
+        }
+
+        public double Size()
+        {
+            double size = Math.Pow(side, 2);
+
+            return size;
+        }
+    }
+      
+}
