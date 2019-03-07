fc4d90c536aaa3f5255d63e2e48859bf779a7d35
brdeleuran <46991809+brdeleuran@users.noreply.github.com>
2019-03-07 12:14:30 +0100

    Add files via upload

---
+++ Queue FIFO/Queue FIFO/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue_FIFO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MenuGUI();
+        }
+
+        static void MenuGUI()
+        {
+            Console.WriteLine("==================================================");
+            Console.WriteLine("             H1 Queue Operations Menu");
+            Console.WriteLine("==================================================");
+            Console.ReadKey();
+        }
+    }
+}
