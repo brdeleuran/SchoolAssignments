35905e40fdd515ee114fef881a9378d5eab72632
brdeleuran <46991809+brdeleuran@users.noreply.github.com>
2019-05-26 17:40:44 +0200

    Add files via upload

---
+++ ThreadBottle/ThreadBottle/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadBottle
+{
+    class Program
+    {
+        static Threads tr = new Threads();
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Thread trProducer = new Thread(new ThreadStart(tr.ProducerThread));
+                Thread trSplitter = new Thread(new ThreadStart(tr.ConsumerSplitterThread));
+                Thread trBeer = new Thread(new ThreadStart(tr.BeerConsumerThread));
+                Thread trSoda = new Thread(new ThreadStart(tr.SodaConsumerThread));
+                trProducer.Start();
+                trProducer.Join();
+                trSplitter.Start();
+                trSplitter.Join();
+                trBeer.Start();
+                trBeer.Join();
+                trSoda.Start();
+                trSoda.Join();
+
+                Thread.Sleep(4000);
+            }
+        }
+    }
+}
---
+++ ThreadBottle/ThreadBottle/Threads.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadBottle
+{
+    class Threads
+    {
+        public string[] beerSodaBuffer = new string[6];
+        public string[] beerBuffer = new string[3];
+        public string[] sodaBuffer = new string[3];
+        int runNumber = 0;
+        public void ProducerThread()
+        {
+            try
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    runNumber++;
+                    beerSodaBuffer[i] = "Beer";
+                    Console.WriteLine("Added beer");
+                }
+                for (int i = 3; i < 6; i++)
+                {
+                    beerSodaBuffer[i] = "Soda";
+                    Console.WriteLine("Added Soda");
+                }
+                Console.WriteLine("Producer Thread Done");
+                Thread.Sleep(500);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("Closing program");
+                Environment.Exit(0);
+            }
+        }
+
+        public void ConsumerSplitterThread()
+        {
+            try
+            {
+                for (int i = 0; i < beerSodaBuffer.Length; i++)
+                {
+                    if (beerSodaBuffer[i] == "Beer")
+                    {
+                        beerBuffer[i] = "Beer";
+                        beerSodaBuffer[i] = "";
+                    }
+                    else if (beerSodaBuffer[i] == "Soda")
+                    {
+                        sodaBuffer[i - 3] = "Soda";
+                        beerSodaBuffer[i] = "";
+                    }
+                    Thread.Sleep(500);
+                }
+                Console.WriteLine("Splitter Thread Done");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("Closing program");
+                Environment.Exit(0);
+            }
+        }
+
+        public void BeerConsumerThread()
+        {
+            try
+            {
+                for (int i = 0; i < beerBuffer.Length; i++)
+                {
+                    if (beerBuffer[i] == "Beer")
+                    {
+                        beerBuffer[i] = "";
+                        Console.WriteLine("Removed Beer");
+                    }
+                    else
+                        Thread.Sleep(500);
+
+                }
+                Console.WriteLine("Beer Consumer Thread Done");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("Closing program");
+                Environment.Exit(0);
+            }
+        }
+
+        public void SodaConsumerThread()
+        {
+            try
+            {
+                for (int i = 0; i < sodaBuffer.Length; i++)
+                {
+                    if (sodaBuffer[i] == "Soda")
+                    {
+                        sodaBuffer[i] = "";
+                        Console.WriteLine("Removed Soda");
+                    }                    
+                    else
+                        Thread.Sleep(500);
+                }
+                Console.WriteLine("Soda Consumer Thread Done");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("Closing program");
+                Environment.Exit(0);
+            }
+        }
+
+
+    }
+}
