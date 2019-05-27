e7f19b3af04079bcbce4e45979ae3bc54a843b5a
brdeleuran <46991809+brdeleuran@users.noreply.github.com>
2019-05-27 10:28:11 +0200

    Add files via upload

---
+++ ConsumerProducerThreading/ConsumerProducerThreading/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsumerProducerThreading
+{
+    class Program
+    {
+        static thr thr = new thr();
+        static void Main(string[] args)
+        {
+            //while (true)
+            //{
+            //    for (int i = 0; i < 6; i++)
+            //    {
+            //        Thread threadProduce = new Thread(new ThreadStart(thr.Producer));
+            //        threadProduce.Start();
+            //    }
+            //    Thread.Sleep(2000);
+            //    for (int i = 0; i < 6; i++)
+            //    {
+            //        Thread thrConsume = new Thread(new ThreadStart(thr.Consumer));
+            //        thrConsume.Start();
+            //    }
+            //    Thread.Sleep(2000);
+            //}
+
+            while (true)
+            {
+                Thread thrprod1 = new Thread(new ThreadStart(thr.Producer1));
+                Thread thrCons1 = new Thread(new ThreadStart(thr.Consumer1));
+                thrprod1.Start();
+                thrCons1.Start();
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
---
+++ ConsumerProducerThreading/ConsumerProducerThreading/thr.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsumerProducerThreading
+{
+    class thr
+    {
+        static int value = 0;
+        static object key = new object();
+        private bool block = true;
+        public void Producer()
+        {
+            try
+            {
+                if (value <= 3)
+                {
+                    value++;
+                    Console.WriteLine("Producer added 1, current amount: " + value);
+                    
+                }
+                else
+                {
+                    Console.WriteLine("Producer was not allowed to add, current amount: " + value);
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public void Consumer()
+        {
+            try
+            {
+                if (value > 0)
+                {
+                    value--;
+                    Console.WriteLine("Consumer removed 1, current amount: " + value);
+                }
+                else
+                {
+                    Console.WriteLine("Consumer was not allowed to remove, current value: " + value);
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public void Producer1()
+        {
+            try
+            {
+                lock (key)
+                {
+                    value++;
+                    Console.WriteLine("Producer added 1, current amount: " + value);
+                    block = true;
+                    while (block)
+                    {
+                        Monitor.Wait(key);
+                        Console.WriteLine("Producer Waits..");
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public void Consumer1()
+        {
+            try
+            {
+                lock (key)
+                {
+                    value--;
+                    Console.WriteLine("Consumer removed 1, current amount: " + value);
+
+                    block = false;
+                    Monitor.Pulse(key);
+
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
